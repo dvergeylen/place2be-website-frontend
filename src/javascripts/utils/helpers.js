7d8f44bc716db1760d70999144a9b5a65e46d2ef
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2020-05-25 17:01:47 +0200

    refactor: added fetchData helper function

--- src/javascripts/utils/helpers.js
+++ src/javascripts/utils/helpers.js
@@ -1,10 +1,26 @@
-const path2url = (path) => {
+function path2url(path) {
   const sanitized = (Array.isArray(path)
     ? path : [path]).map((s) => s.replace(/^\/+/, ''));
 
   return `${window.location.protocol}//${window.location.host}/${sanitized.join('/')}`;
-};
+}
+
+/* Usage:
+ * const res = await fetchData(token, url);
+ * → res.status(): 200, 400, ...
+ * → res.statusText(): 'Ok', 'Bad Request', ...
+ * → await res.json(): Json body
+ */
+function fetchData(token, url) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Response
+  return fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
 
 export {
   path2url,
+  fetchData,
 };
