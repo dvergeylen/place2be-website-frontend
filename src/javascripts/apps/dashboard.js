dd5ec8a2c9dabdf569dcad2e1443364d9173dae5
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2020-05-29 11:17:33 +0200

    feat: specified userId dashboard has to render
    This allows a common user to only see its own dashboard
    and admin users to see other user's dashboard.
    
    When a user_id param is found in the URL, one will use this one
    when calling the API.
    When none is found, it will use the user_id cookie.
    
    When the API calls are done, a verification is done backend to confirm
    a user has the correct permissions to see someone else's dashboard,
    via the JWT claims sent when doing the requests (httponly, invisible to javascript
    but sent anyway).

--- src/javascripts/apps/dashboard.js
+++ src/javascripts/apps/dashboard.js
@@ -5,6 +5,9 @@
 /* config */
 import config from '../../../local-config.json'; // ⚠️ Not commited config
 
+/* Javascripts */
+import { getCookie } from '../utils/helpers';
+
 /* Stylesheets */
 import '../../stylesheets/navbar.scss';
 import '../../stylesheets/dashboard.scss';
@@ -14,6 +17,14 @@ import Dashboard from '../../components/Dashboard/Dashboard.svelte';
 import CredentialsButtons from '../../components/Credentials/CredentialsButtons.svelte';
 
 const app = (async () => {
+
+  // Figure out: are we asking our own dashboard
+  // or a distinct user's one?
+  const params = new URLSearchParams(window.location.search);
+  const userId = params.has('user_id')
+    ? params.get('user_id')
+    : getCookie(document.cookie, 'user_id');
+
   // eslint-disable-next-line no-unused-vars
   const auth = new CredentialsButtons({
     target: document.getElementById('credentials-buttons'),
@@ -29,6 +40,7 @@ const app = (async () => {
     props: {
       apiProtocol: config.api.protocol,
       apiHost: config.api.host,
+      userId,
     },
   });
 })();
